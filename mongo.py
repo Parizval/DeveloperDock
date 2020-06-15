84eb3636ff99475498a94196994aa31cfdc0b35b
Anmol Goyal <anmolgoyal@gmail.com>
2020-06-15 16:16:46 +0530

    added MongoDb Setup and Auth Functionality

---
+++ mongo.py
@@ -0,0 +1,53 @@
+from passlib.hash import pbkdf2_sha256
+import pymongo
+import uuid
+
+# Establishing the connection
+username = "DevDock"
+password = "afKNiamzY7us3Il7"
+
+srv = "mongodb+srv://{}:{}@supplychain-u6nhl.mongodb.net/test?retryWrites=true&w=majority".format(
+    username, password)
+client = pymongo.MongoClient(srv)
+
+print("MongoDB Connected")
+
+db = client['DeveloperDock']
+
+LoginCollection = db['Auth']
+
+
+def Register(email,name,password):
+    q1 = {"email":email}
+
+    result1 = LoginCollection.find(q1)
+    check = False
+
+    for  i in  result1:
+        if  email == i['email']:
+            check = True
+            break
+    
+    if check:
+        print("Email Address already exist in database.")
+        return False
+    else:
+        password = pbkdf2_sha256.hash(password)
+        q2 = {"name": name, "email": email,
+              "password": password}
+        
+        LoginCollection.insert_one(q2)
+        return True
+
+def Login(email,password)
+    l1 = {"email": email}
+    res = LoginCollection.find(l1)
+
+    data = {}
+    data['check'] = False
+    for i in res:
+        if pbkdf2_sha256.verify(password, i['password']):
+            data['name'] = i['name']
+
+    print(data)
+    return data
